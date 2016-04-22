578c104accf24dcdafa8cf27e32b355e268ec465
root <root@tools.norcaltechs.com>
2016-04-22 01:49:27 -0700

    updated files

--- pirate_bartender.py
+++ pirate_bartender.py
@@ -1,49 +1,53 @@
 #! /usr/bin/python
 
-
 import random
 
-questions = {
-    "strong": "Do ye like yer drinks strong?",
-    "salty": "Do ye like it with a salty tang?",
-    "bitter": "Are ye a lubber who likes it bitter?",
-    "sweet": "Would ye like a bit of sweetness with yer poison?",
-    "fruity": "Are ye one for a fruity finish?",
-}
 
+answers = {}
+
+questions = {
+    "strong": "Do ye like yer drinks strong? ",
+    "salty": "Do ye like it with a salty tang? ",
+    "bitter": "Are ye a lubber who likes it bitter? ",
+    "sweet": "Would ye like a bit of sweetness with yer poison? ",
+    "fruity": "Are ye one for a fruity finish? "
+}
 ingredients = {
     "strong": ["glug of rum", "slug of whisky", "splash of gin"],
     "salty": ["olive on a stick", "salt-dusted rim", "rasher of bacon"],
     "bitter": ["shake of bitters", "splash of tonic", "twist of lemon peel"],
     "sweet": ["sugar cube", "spoonful of honey", "spash of cola"],
-    "fruity": ["slice of orange", "dash of cassis", "cherry on top"],
+    "fruity": ["slice of orange", "dash of cassis", "cherry on top"]
 }
 
-def find_preferences():
-    preferences = {}
-    for type, question in questions.items():
-        print(question)
-        preferences[type] = input().lower() in ["y", "yes"]
-        print("")
-    return preferences
 
-def make_drink(preferences):
-    drink = []
-    for ingredient_type, liked in preferences.items():
-        if not liked:
-            continue
-
-        drink.append(random.choice(ingredients[ingredient_type]))
-    return drink
+def ask_questions():
+    """
+    Iterate over each question in the questions dictionary.
+    Each response will be added to the the answers dictionary.
+    """
+    print "Answer each question ('y' or 'n') to determine your drink."
+    for key, value in questions.items():
+        reply = raw_input(value).lower()
+        if reply == "y":
+            answers[key] = True
+        else:
+            answers[key] = False
+    return answers
 
 
-def main():
-    preferences = find_preferences()
-    drink = make_drink(preferences)
-    print("One drink coming up.")
-    print("It's full of good stuff.  The recipe is:")
-    for ingredient in drink:
-        print("A {}".format(ingredient))
+def create_drink(answers, ingredients):
+    """
+    Create the drink based on the answers given
+    by matching answers and selecting a random matching ingredient.
+    """
+    print "\nHere's your drink!"
+    for key, value in answers.items():
+        if value is True:
+            print "{0}".format(ingredients[key][random.randint(0, 2)])
+    return
 
-if __name__ == "__main__":
-    main()
+
+if __name__ == '__main__':
+    ask_questions()
+    create_drink(answers, ingredients)
