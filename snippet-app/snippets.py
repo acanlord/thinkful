e9e7438e52f265ca5fab435c4a2efed0f96ed6a2
Richard <rdhouck@gmail.com>
2016-05-07 22:07:05 +0000

    updated snippetswq

--- snippet-app/snippets.py
+++ snippet-app/snippets.py
@@ -25,12 +25,30 @@ def get(name):
 
 
 def main():
+
 	"""Main function"""
+	
 	logging.info("Constructin parser")
 	parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
+	subparsers = parser.add_subparsers(dest="command", help="Available commands")
+	
+	# Subparser for the put command
+	logging.debug("Constructing put subparser")
+	put_parser = subparsers.add_parser("put", help="Store a snippet")
+	put_parser.add_argument("name", help="Name of the snippet")
+	put_parser.add_argument("snippet", help="Snippet text")
+
 	arguments = parser.parse_args()
+        # convert parsed arguments from Namespace to dictionary
+        arguments = vars(arguments)
+        command = arguments.pop("command")
 
-
+        if command == "put":
+                name, snippet = put(**arguments)
+                print("stored {!r} as {!r}".format(snippet, name))
+        elif command == "get":
+                snipet = get(**arguments)
+                print("Retrieved snippet: {!r}".format(snippet))
 
 if __name__ == "__main__":
 	main()
