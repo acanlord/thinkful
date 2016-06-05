c799d562cb2853dce43ddb12ec7bdfa4bb47d917
Richard <rdhouck@gmail.com>
2016-06-05 17:50:52 +0000

    updated hello_world

--- flask_hello_world/hello_world.py
+++ flask_hello_world/hello_world.py
@@ -8,19 +8,23 @@ app = Flask(__name__)
 def say_hi():
 	return "Hello World!"
 
+@app.route("/jedi/<firstname>/<lastname>")
+def jedi(firstname,lastname):
+    return lastname[0:3]+firstname[0:2]
+
 @app.route("/hello/<name>")
-def hi_person(name):
-    return "Hello {}!".format(name.title())
+def hello_person(name):
+    html = """
+        <h1>
+            Hello {}!
+        </h1>
+        <p>
+            Here's a picture of a kitten.  Awww...
+        </p>
+        <img src="http://placekitten.com/g/200/300">
+    """
+    return html.format(name.title())
 
 if __name__ == "__main__":
 	app.run(host=environ['IP'],
 		port=int(environ['PORT']))
-
-def twist(twister):
-    def decorator(function):
-        @functools.wraps(function)
-        def wrapper(*args, **kwargs):
-            print(twister)
-            function(*args, **kwargs)
-        return wrapper
-    return decorator
