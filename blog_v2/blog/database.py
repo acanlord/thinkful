8d6e55d7c798909eb4a608d9c9359b16e40879ea
Richard <rdhouck@gmail.com>
2016-07-02 19:10:10 +0000

    updated blog_v2

--- blog_v2/blog/database.py
+++ blog_v2/blog/database.py
@@ -1,3 +1,5 @@
+import datetime
+from sqlalchemy import Column, Integer, String, Text, DateTime
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
@@ -8,3 +10,13 @@ engine = create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 session = Session()
+
+class Entry(Base):
+    __tablename__ = "entries"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(1024))
+    content = Column(Text)
+    datetime = Column(DateTime, default=datetime.datetime.now)
+
+Base.metadata.create_all(engine)
